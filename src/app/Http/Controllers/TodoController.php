7fabad2b6724b54ce26e7d35b24c5274cd0b57de
kurumi107 <maruyama07261977@gmail.com>
2024-04-26 18:20:52 +0900

    DBにデータを保存する。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
+use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
@@ -15,7 +16,17 @@ class TodoController extends Controller
     }
 
     public function create()
-{
-    return view('todo.create'); 
+    {
+        return view('todo.create'); 
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content'); 
+        $todo = new Todo(); 
+        $todo->content = $content;
+        $todo->save();
+        return redirect()->route('todo.index');
+    }
 }
-}
+
