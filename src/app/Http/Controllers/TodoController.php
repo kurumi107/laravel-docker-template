883ca611b6ff85dde839db380c56991bdaa0c667
kurumi107 <maruyama07261977@gmail.com>
2024-05-06 23:50:28 +0900

    add:編集機能の実装。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -43,11 +43,22 @@ class TodoController extends Controller
     }
 
     public function edit($id)
-{
+    {
     // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
-    $todo = $this->todo->find($id);
-    return view('todo.edit', ['todo' => $todo]);
-}
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+    // TODO: リクエストされた値を取得
+        $inputs = $request->all();
+        $todo = $this->todo;
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
+
+    }
 
 
 
