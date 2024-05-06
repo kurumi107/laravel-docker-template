d381c6e47d0a0ce9ba72db02ff7051feb045988b
kurumi107 <maruyama07261977@gmail.com>
2024-05-07 00:01:11 +0900

    feat:バリデーション機能実装。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Todo;
-use Illuminate\Http\Request;
+use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
@@ -21,7 +21,7 @@ class TodoController extends Controller
         return view('todo.create'); 
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all(); 
 
@@ -49,7 +49,7 @@ class TodoController extends Controller
         return view('todo.edit', ['todo' => $todo]);
     }
 
-    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    public function update(TodoRequest $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
     {
     // TODO: リクエストされた値を取得
         $inputs = $request->all();
